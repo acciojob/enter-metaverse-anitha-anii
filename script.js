3c3122d24cd3aa455789eaebb783d65345acaf62
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-15 08:40:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let button = document.getElementById('enterBtn');
+let para = document.getElementById('status');
+let body = document.body;
+
+button.addEventListener('click', function () {
+  let header = document.createElement('h1');
+  header.innerText = "Entered Metaverse";
+
+  if (body.contains(para)) {
+    body.replaceChild(header, para);
+  }
+});
+
