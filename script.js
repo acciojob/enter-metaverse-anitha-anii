943c324ca0b5e0ade694c3465fc7a55bf7c426bd
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-22 18:26:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 
-const statusElement = document.getElementById("status");
-const enterButton = document.getElementById("enterBtn");
-let head = document.getElementById('status');
 
- enterButton.addEventListener("click", function() {
-   const h1Element = document.createElement("h1");
-  h1Element.id ="status";
-    h1Element.textContent = "Entered Metaverse";
-	 head.appendChild(h1Element);
-     statusElement.parentNode.replaceChild(h1Element, statusElement);
-});
-
+ function updateStatus() {
+      document.getElementById("status").innerHTML = "<h1>Entered Metaverse</h1>";
+    }
 
 
 
